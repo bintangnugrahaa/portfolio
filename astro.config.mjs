0c2bcab38b7c7a3843b41936054deb3119fe3d1d
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2025-10-03 17:02:03 +0700

    refactor: simplify build minification settings in `astro.config.mjs`
    - Changed minification setting from Terser with specific options to a boolean value for simplicity.

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,13 +12,7 @@ export default defineConfig({
       },
     },
     build: {
-      minify: "terser",
-      terserOptions: {
-        compress: {
-          drop_console: true,
-          drop_debugger: true,
-        },
-      },
+      minify: true,
     },
   },
   output: "static",
