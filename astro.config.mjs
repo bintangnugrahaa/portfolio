975a5373184a5d977af573229ff305cf866b9738
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2026-03-19 01:12:56 +0700

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@astrojs/react";
 
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [react()],
   vite: {
+    plugins: [tailwindcss()],
     resolve: {
       alias: {
         "@": "/src",
