209c19c12218e7ee9789806de9df1579ba6ebe14
Muhammad Bintang Nugraha <djakarta1973@gmail.com>
2026-05-03 13:37:37 +0700

    feat: optimize performance, accessibility and seo for lighthouse perfection

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,15 @@ import tailwindcss from '@tailwindcss/vite';
 
 import icon from 'astro-icon';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  // site: '#',
-  // base: '/',
+  site: 'https://bintangnugraha.my.id',
+  base: '/',
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [icon()]
+  integrations: [icon(), sitemap()]
 });
